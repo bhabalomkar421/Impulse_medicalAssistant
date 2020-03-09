45580967de5e4c872d1ffe7c51ea93f9d1655c02
Elysian Spirit <abhig0209@gmail.com>
2020-03-09 22:11:06 +0530

    Transmission Completed

--- Arduino/Traffic Light Control/RF Transmitter and Receiver/Receiver/Receiver.ino
+++ Arduino/Traffic Light Control/RF Transmitter and Receiver/Receiver/Receiver.ino
@@ -1,25 +1,22 @@
-/*
- RF433Mhz Receiver Code
- https://circuits4you.com
- Receiver is connected on PIN 11
-
- Arduino                     Receiver
-  GND--------------------------GND
-  D11--------------------------Data
-  5V---------------------------VCC
-*/
-
 #include <RH_ASK.h>
-#include <SPI.h> // Not actualy used but needed to compile
+#ifdef RH_HAVE_HARDWARE_SPI
+#include <SPI.h> 
+#endif
 
-RH_ASK driver;
-// RH_ASK driver(2000, 2, 4, 5); // ESP8266: do not use pin 11
-
+// connect data to pin 2
+// ( Speed, receving pin , Transmitting pin , push to talk)
+RH_ASK driver(2000, 2, 4, 5); 
 void setup()
 {
-    Serial.begin(9600);  // Debugging only
+#ifdef RH_HAVE_SERIAL
+    Serial.begin(9600);    
+#endif
     if (!driver.init())
+#ifdef RH_HAVE_SERIAL
          Serial.println("init failed");
+#else
+  ;
+#endif
 }
 
 void loop()
@@ -29,14 +26,13 @@ void loop()
 
     if (driver.recv(buf, &buflen)) // Non-blocking
     {
-  int i;
-
-  // Message with a good checksum received, dump it.
-  driver.printBuffer("Got:", buf, buflen);
-        /* Use this in case hex values are displayed
-          Serial.print("Received:");
-          for (i = 0; i < buflen; i++) Serial.print(buf[i]);
-          Serial.println("");
-        */
-     }
+      // Message with a good checksum received, dump it.
+      //driver.printBuffer("Got:", buf, buflen);
+        
+        String rev;
+          for (int i = 0; i < buflen;i++) {
+            rev += (char)buf[i];
+          }
+          Serial.println(rev);
+    }
 }
--- Arduino/Traffic Light Control/RF Transmitter and Receiver/Transmitter/Transmitter.ino
+++ Arduino/Traffic Light Control/RF Transmitter and Receiver/Transmitter/Transmitter.ino
@@ -1,34 +1,29 @@
-/*
-433MHz Transmitter Code
-https://circuits4you.com
-Transmitter is connected on PIN 12
-
- Arduino                     Transmitter
-  GND--------------------------GND
-  D12--------------------------Data
-  5V---------------------------VCC
-
-*/
-
 #include <RH_ASK.h>
-#include <SPI.h> // Not actually used but needed to compile
+#ifdef RH_HAVE_HARDWARE_SPI
+#include <SPI.h> 
+#endif
 
-RH_ASK driver;
-// RH_ASK driver(2000, 2, 4, 5); // ESP8266: do not use pin 11
-
+// connect data to pin 2
+// ( Speed, receving pin , Transmitting pin , push to talk)
+RH_ASK driver(2000, 4, 2, 5);
 void setup()
 {
-    Serial.begin(9600);    // Debugging only
+#ifdef RH_HAVE_SERIAL
+    Serial.begin(9600);    
+#endif
     if (!driver.init())
+#ifdef RH_HAVE_SERIAL
          Serial.println("init failed");
+#else
+  ;
+#endif
 }
 
 void loop()
 {
-    const char *msg = "hello";
+    const char *msg = "hello God";
 
     driver.send((uint8_t *)msg, strlen(msg));
     driver.waitPacketSent();
     delay(200);
-    Serial.println("Working !!");
-}
+} 
