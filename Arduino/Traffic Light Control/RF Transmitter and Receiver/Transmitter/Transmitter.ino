c2ff900dda3d2dceabb491f61b39b8df94992548
Elysian Spirit <abhig0209@gmail.com>
2020-03-13 20:30:04 +0530

    eligit 2.1

--- Arduino/Traffic Light Control/RF Transmitter and Receiver/Transmitter/Transmitter.ino
+++ Arduino/Traffic Light Control/RF Transmitter and Receiver/Transmitter/Transmitter.ino
@@ -5,7 +5,14 @@
 
 // connect data to pin 2
 // ( Speed, receving pin , Transmitting pin , push to talk)
-RH_ASK driver(2000, 4, 2, 5);
+RH_ASK driver(2000, 4, 10, 5);
+
+
+int sw1 = 9;
+int sw2 = 7;
+int sw3 = 5;
+int sw4 = 3;
+
 void setup()
 {
 #ifdef RH_HAVE_SERIAL
@@ -17,13 +24,55 @@ void setup()
 #else
   ;
 #endif
+
+
+
+pinMode(sw1,INPUT_PULLUP);
+pinMode(sw2,INPUT_PULLUP);
+pinMode(sw3,INPUT_PULLUP);
+pinMode(sw4,INPUT_PULLUP);
 }
 
 void loop()
 {
-    const char *msg = "hello God";
-
+  int a = digitalRead(sw1);
+  int b = digitalRead(sw2);
+  int c = digitalRead(sw3);
+  int d = digitalRead(sw4);
+  
+  if ( a == 0 && b!= 0 && c != 0 && d!=0){
+    const char *msg = "East"; 
     driver.send((uint8_t *)msg, strlen(msg));
-    driver.waitPacketSent();
-    delay(200);
+  driver.waitPacketSent();
+  delay(200);
+  }
+  else if (a != 0 && b== 0 && c != 0 && d!=0){
+    const char *msg = "South"; 
+    driver.send((uint8_t *)msg, strlen(msg));
+  driver.waitPacketSent();
+  delay(200);
+  }
+  else if (a != 0 && b!= 0 && c == 0 && d!=0){
+    const char *msg = "West"; 
+    driver.send((uint8_t *)msg, strlen(msg));
+  driver.waitPacketSent();
+  delay(200);
+  }
+  else if (a != 0 && b!= 0 && c != 0 && d==0){
+    const char *msg = "North"; 
+    driver.send((uint8_t *)msg, strlen(msg));
+  driver.waitPacketSent();
+  delay(200);
+  }
+  else 
+  {
+   const char *msg = "Invalid Input";
+   driver.send((uint8_t *)msg, strlen(msg));
+  driver.waitPacketSent();
+  delay(200);
+  }
+  
+    
+
+    
 } 
--- Arduino/Traffic Light Control/TrafficLight/TrafficLight.ino
+++ Arduino/Traffic Light Control/TrafficLight/TrafficLight.ino
@@ -62,34 +62,3 @@ void loop() {
 
   
 }
-
-
-//void changeLane()
-//{
-//   digitalWrite (redled, 1);
-// delay(2000);
-//    digitalWrite (redled, 0); //stopped  
-//      digitalWrite (yellowled, 1); 
-//   delay (2000); // yellowled lights for 3 sec.
-//   digitalWrite (yellowled, 0);
-//    digitalWrite (greenled, 1);
-//   delay (2000);
-//   digitalWrite (greenled, 0);// greenled start flashing
-//   delay (500);
-//   digitalWrite (greenled, 1);
-//   delay (500);    
-//   digitalWrite (greenled, 0);
-//   delay (500);
-//   digitalWrite (greenled, 1);
-//   delay (500);
-//   digitalWrite (greenled, 0);
-//  delay (500);
-// digitalWrite (greenled, 1);
-//   delay (500);
-//   digitalWrite (greenled, 0);
-//     delay (500);
-// digitalWrite (greenled, 1);
-//   delay (500);
-//   digitalWrite (greenled, 0); 
-//}
-//
