25013663e2001e309bfde103e0e7c33a7484b1a3
Elysian Spirit <abhig0209@gmail.com>
2020-03-22 15:09:50 +0530

    Text Feature Extraction completed

--- Prediction/textRecognitionOCR/testRecognize.py
+++ Prediction/textRecognitionOCR/testRecognize.py
@@ -2,11 +2,12 @@ import cv2 as cv
 from PIL import Image
 import re
 import pytesseract
+
 im = Image.open("lab.png")
-text = pytesseract.image_to_string(im,lang="eng")
+text = pytesseract.image_to_string(im, lang="eng")
 # print(text)
-name = re.findall(r"Name",text)
-#print(name)
+name = re.findall(r"Name", text)
+# print(name)
 li = []
 li1 = []
 li = text.split("\n")
