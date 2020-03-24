213ea4ea54b90023ae38f428d7f5fc70be5a3b6b
bhabalomkar421 <omkarbhabal11@gmail.com>
2020-03-25 00:23:49 +0530

    DiseasePredictionProbability on going

---
+++ ML Frontent/templates/DiseasePredictionProbability/disPredProb.js
@@ -0,0 +1,17 @@
+// var select = document.querySelector("#pk_sdk_symptoms");
+
+// // select.addEventListener("click",function(){
+// // 	select.style.backgroundColor='#ffff00';
+// // 
+// // 	});
+
+// var v1 = document.getElementById("pk_sdk_symptoms");
+// v1.addEventListener("click", myFunction);
+
+// function myFunction() {
+//   	v1.style.background='#ffff00';
+//   	console.log("btn clicked");
+// }
+document.getElementById("pk_sdk_symptoms").addEventListener("click",function(){
+	this.style.backgroundColor='#ffff00';
+});
