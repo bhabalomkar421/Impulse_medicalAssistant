9dd34874de5f9939a6ff8af28678cc3fa1711cdc
bhabalomkar421 <omkarbhabal11@gmail.com>
2020-03-26 20:06:01 +0530

    pdf_To_image convertor done

---
+++ ML Frontend/templates/pdf_to_imageConvertor/pdf_To_image.py
@@ -0,0 +1,10 @@
+import tempfile
+from pdf2image import convert_from_path
+from pdf2image.exceptions import (
+	PDFInfoNotInstalledError,
+	PDFPageCountError,
+	PDFSyntaxError
+)
+with tempfile.TemporaryDirectory() as path:
+	images_from_path = convert_from_path('/home/omkar/Documents/Impulse_medicalAssistant/ML Frontend/templates/pdf_to_imageConvertor/pdfTest.pdf', output_folder='/home/omkar/Documents/Impulse_medicalAssistant/ML Frontend/templates/pdf_to_imageConvertor')
+	print("working")
