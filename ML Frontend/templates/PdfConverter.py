0243db7846b9206eff38063feb6653c26411ca1b
Elysian Spirit <abhig0209@gmail.com>
2020-03-26 20:30:12 +0530

    current status problem solved

---
+++ ML Frontend/templates/PdfConverter.py
@@ -0,0 +1,11 @@
+import tempfile
+from pdf2image import convert_from_path
+from pdf2image.exceptions import (
+    PDFInfoNotInstalledError,
+    PDFPageCountError,
+    PDFSyntaxError
+)
+with tempfile.TemporaryDirectory() as path:
+    images_from_path = convert_from_path(
+        'D:\\IT\\Hackathon\\Impulse\\ML Frontend\\templates\\pdfTest.pdf', output_folder='D:\\IT\\Hackathon\\Impulse\\ML Frontend\\Received_Files')
+    print("working")
